8bb61ea361ab75cbee5b9c1f956c6197918d486a
sebastianojeda <sebastianojeda.school@gmail.com>
2015-04-20 13:21:09 -0700

    initial commit hope i did this quiz right

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Event setup using a convenience method
+$( ".dan" ).click(function() {
+    alert( "Hey you clicked on me" );
+});
+
+// Event setup using a convenience method
+$( ".dan" ).click(function() {
+    alert( "Hey you clicked on me again" );
+});
+
